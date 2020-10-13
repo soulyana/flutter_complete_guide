2744554a6c77e5c54d27ecfb81fcabb8ac710cd9
soulyana <soulyana.lakew@accenturefederal.com>
2020-10-13 17:46:19 -0400

    class constructors & names arguments, additional syntax

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+// void main() {
+//  runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
+  @override // decorator provided by dart to make code clear of overiding build method. common practice
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
