5559eff1749c1987253f63515a46ea8e6ebbeec7
soulyana <soulyana.lakew@accenturefederal.com>
2020-10-13 18:27:32 -0400

    adding layout widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,23 @@ class MyApp extends StatelessWidget {
   @override // decorator provided by dart to make code clear of overiding build method. common practice
   Widget build(BuildContext context) {
     // TODO: implement build
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(child: Text('Answer 1'), onPressed: null),
+            RaisedButton(child: Text('Answer 1'), onPressed: null),
+            RaisedButton(child: Text('Answer 1'), onPressed: null),
+          ],
+        ),
       ),
     );
   }
